685cf004d40a69aa4b0ae86b4f90292de8aef875
BiswajitThakur <biswajit733127@gmail.com>
2020-10-29 12:06:58 +0530

    Bug fixes....

--- public/verbs/src/js/verbs2.js
+++ public/verbs/src/js/verbs2.js
@@ -1,13 +1,14 @@
 'use strict';
-
+(function(){
+  let dataX='';
 for(let a=0; a<data.length; a++){
-    document.write('<tr>');
-    document.write('<td>'+(a+1)+'<\/td>');
-    document.write('<td>'+data[a].bn+'<\/td>');
-    document.write('<td>'+data[a].v1+'<\/td>');
-    document.write('<td>'+data[a].v2+'<\/td>');
-    document.write('<td>'+data[a].v3+'<\/td>');
-    document.write('<\/tr>');
+    dataX+='<tr>';
+    dataX+='<td style="color:yellow;">'+parseInt(a+1)+'<\/td>';
+    dataX+='<td>'+data[a].bn+'<\/td>';
+    dataX+='<td>'+data[a].v1+'<\/td>';
+    dataX+='<td>'+data[a].v2+'<\/td>';
+    dataX+='<td>'+data[a].v3+'<\/td>';
+    dataX+='<\/tr>';
 };
-
-
+document.getElementById('tbody').innerHTML=dataX;
+}());
